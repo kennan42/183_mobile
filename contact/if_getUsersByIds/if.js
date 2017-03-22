735d6edb4a6a403c37d9dfb6dabc5f6eb78ddf43
ken <ken@kendeMacBook-Pro.local>
2017-03-22 17:21:33 +0800

    format

--- contact/if_getUsersByIds/if.js
+++ contact/if_getUsersByIds/if.js
@@ -17,32 +17,32 @@ function run(Param, Robot, Request, Response, IF) {
     var conn = mongoose.createConnection(global.mongodbURL);
     var baseUserModel = conn.model("base_user", contactSchema.BaseUserSchema);
     baseUserModel.find({
-        "PERNR" : {
-            $in : userIds
+        "PERNR": {
+            $in: userIds
         }
     }, {
-        "PERNR" : 1,
-        "NACHN" : 1,
-        "GESCH_T" : 1,
-        "ORGEH" : 1,
-        "ORGTX" : 1,
-        "PLSTX":1,
-        "photoStatus" : 1,
-        "photoURL" : 1,
-        "photoURL2" : 1,
-		"photoNewURL":1,
-		"photoNewStatus":1
+        "PERNR": 1,
+        "NACHN": 1,
+        "GESCH_T": 1,
+        "ORGEH": 1,
+        "ORGTX": 1,
+        "PLSTX": 1,
+        "photoStatus": 1,
+        "photoURL": 1,
+        "photoURL2": 1,
+        "photoNewURL": 1,
+        "photoNewStatus": 1
     }, function(err, data) {
         conn.close();
         console.log("contact.getUsersByIds end");
-		 if (data.photoNewStatus != null) {
-					
-                    data.photoURL = data.photoNewURL;
-                    data.photoURL2 = data.photoNewURL;
-                }
+        if (data.photoNewStatus != null) {
+
+            data.photoURL = data.photoNewURL;
+            data.photoURL2 = data.photoNewURL;
+        }
         Response.end(JSON.stringify({
-            "status" : "0",
-            "data" : data
+            "status": "0",
+            "data": data
         }));
     });
 }
@@ -60,4 +60,3 @@ function handleUserIds(userIds) {
 }
 
 exports.Runner = run;
-
